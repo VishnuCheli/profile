175f4745598485f45637af39b320ac8ee80d11d4
VishnuCheli <vishnucheliayan@gmail.com>
2024-04-16 21:31:55 -0400

    Created home and about page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,24 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className ='w-full h-screen bg-[#0a192f] '>
-        
+    <div name='home' className ='w-full h-screen bg-[#0a192f] '>
+      {/*container*/}
 
+      <div className = 'max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+        <p className='text-pink-300'> Hello, my name is</p>
+        <h1 className='text-4xl sm:text-5xl font-bold text-[#ccd6f6]'>Vishnu Neduncheliyan</h1>
+        <h2 className='text-4xl sm:text-4xl font-bold text-[#8892b0] '>Full Stack Developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>Brief...</p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-300 hover:border-pink-300'>
+            View Work 
+          <span className='group-hover:rotate-90 duration-300'>
+            <HiArrowNarrowRight className='ml-3'/>
+            </span>
+          </button>
+        </div>
+
+      </div>
 
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,5 @@
 import React, {useState} from 'react'
 import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
-import CIcon from '@coreui/icons-react';
-import { cibLeetcode } from '@coreui/icons';
 import LeetcodeLogo from '../assets/image.png';
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
@@ -13,7 +11,7 @@ export const Navbar = () => {
   return (
     <div className = 'fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
-            <img src={Logo} alt = "Logo Image" style = {{width: '50px'}}></img>
+            <img src={Logo} alt = "LogoImage" style = {{ width: '70px'}}></img>
         </div>
     
         <ul className='hidden md:flex'>
@@ -39,7 +37,7 @@ export const Navbar = () => {
         </ul>
 
         {/*social icons*/}
-        <div className='flex fixed flex-col top-[35%] left-0'>
+        <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
             <ul>
                 <li className = 'w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0077B5]'>
                     <a className = 'flex justify-between items-center w-full text-gray-300' href='/'>Linkedin<FaLinkedin size={30}/></a>
