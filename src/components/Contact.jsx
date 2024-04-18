6a5c5696d7de7c598f80b13c0820c644dd2f81e2
VishnuCheli <vishnucheliayan@gmail.com>
2024-04-17 23:44:26 -0400

    added view work scroll and contact info part

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,18 +2,34 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center py-4'>
-        <form method='POST' action="https://getform.io/f/eapdzgva" className='flex flex-col max-w-[600px] w-full'>
-            <div className='pb-8'>
-                <p className='text-4xl font-bold inline border-b-4 border-pink-400 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>( Contact me through this form )</p>
+    <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center py-4 text-black'>
+        {/* grid */}
+        <div className='max-w-[1000px] mx-auto p-4 w-full grid md:grid-cols-2 gap-8'>
+            <form method='POST' action="https://getform.io/f/eapdzgva" className='flex flex-col w-full '>
+                <div className='pb-2 text-center md:text-left'>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-400 text-gray-300'>Contact Form</p>
+                    <p className='text-gray-300 pt-3 pb-2'>( Contact me through this form )</p>
+                </div>
+                <input name="Name" className='placeholder-gray-500 bg-[#ccd6f6] p-2 hover:bg-pink-50 hover:bg-opacity-95' type="text" placeholder='Name *'/>
+                <input name="Email" className='my-4 p-2 placeholder-gray-600 bg-[#ccd6f6] hover:bg-pink-50 hover:bg-opacity-95' type="text" placeholder='E-mail *'/>
+                <textarea name='Message' className='bg-[#ccd6f6] placeholder-gray-600 p-2 hover:bg-pink-50 hover:bg-opacity-95' rows="10" placeholder='Message *'></textarea>
+                <button className='text-white border-2 hover:bg-pink-400 hover:border-pink-400 px-4 py-3 mx-auto my-6'>Send</button>
+            </form>
+            <div className='flex flex-col w-full items-center md:items-start'>
+                <div className='pb-12 '>
+                    <p className='text-4xl font-bold inline border-b-4 border-pink-400 text-gray-300'>Contact Information</p>
+                </div>
+                <div className='bg-[#ccd6f6] p-2 flex w-[300px]'>
+                    <span className='inline-block'>
+                    <p className='font-bold'><span >Current Location:</span> Fairfax, Virginia, USA </p>
+                    <p className='my-4 font-bold'><span>Phone:</span> 7033324907</p>
+                    <p className='font-bold'><span>Email:</span> vishnucheliyan@gmail.com</p>
+                    </span>
+                </div>
             </div>
-            <input name="Name" className='bg-[#ccd6f6] p-2' type="text" placeholder='Name'/>
-            <input name="Email" className='my-4 p-2 bg-[#ccd6f6]' type="text" placeholder='Email'/>
-            <textarea name='Message' className='bg-[#ccd6f6] p-2' rows="10" placeholder='Message'></textarea>
-            <button className='text-white border-2 hover:bg-pink-400 hover:border-pink-400 px-4 py-3 mx-auto my-6'>Send</button>
-        </form>
+        </div>
     </div>
+    // border-b-2 border-b-transparent hover:border-pink-200
   )
 }
 
